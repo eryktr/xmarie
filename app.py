1634a4907182fc8518f1c471d942f2261c34755f
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-10-06 21:36:17 +0200

    Bumped xmarie-vm to 1.1.0, applied interface changes

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ def run():
         raise ValueError('Invalid request type')
     code = request.json.get('code')
     debug = request.json.get('debug')
+    input_ = request.json.get('input')
     try:
-        snapshots = api.run(code, debug=debug)
+        snapshots = api.run(code, debug=debug, input_=input_)
     except Exception as err:
         return jsonify(statusCode=HTTPStatus.INTERNAL_SERVER_ERROR, message=str(err))
     snapshots_dicts = [serializer.serialize_snashot(ss) for ss in snapshots]
-    print(snapshots)
-    return jsonify(statusCode=200, snapshots=snapshots_dicts)
+    return jsonify(statusCode=http.HTTPStatus.OK, snapshots=snapshots_dicts)
