6616c7a97358f6d53f4db54ece172e320423bc34
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-10-14 21:44:31 +0200

    Implemented parsing of breakpoints Bumped pkg version

--- app.py
+++ app.py
@@ -26,37 +26,9 @@ def run():
     debug = req.get('debug')
     input_ = req.get('input')
     breakpoints = req.get('breakpoints')
-    print(_parse_breakpoints(breakpoints, code))
     try:
-        snapshots = api.run(code, debug=debug, input_=input_)
+        snapshots = api.run(code, debug=debug, input_=input_, breakpoints=breakpoints)
     except Exception as err:
         return jsonify(statusCode=HTTPStatus.INTERNAL_SERVER_ERROR, message=str(err))
     snapshots_dicts = [serializer.serialize_snashot(ss) for ss in snapshots]
     return jsonify(statusCode=http.HTTPStatus.OK, snapshots=snapshots_dicts)
-
-
-@dataclass
-class Breakpoint:
-    current_lineno: int
-    original_lineno: int
-    instr: str
-
-
-def _parse_breakpoints(breakpoints: List[int], code: str):
-    new_breakpoints = []
-    code_lines = code.split('\n')
-    code_with_breakpoint_markers = []
-    for lineno, line in enumerate(code_lines):
-        if lineno in breakpoints:
-            code_with_breakpoint_markers.append(f'{lineno}*{line}')
-        else:
-            code_with_breakpoint_markers.append(line)
-    code_with_breakpoint_markers = '\n'.join(code_with_breakpoint_markers)
-    cleaned_code = re.sub(r'\n+', '\n', code_with_breakpoint_markers)
-    cleaned_code_lines = cleaned_code.split('\n')
-    for lineno, line in enumerate(cleaned_code_lines, start=1):
-        match = re.match(r'(\d+)\*(.*)$', line)
-        if match:
-            breakpoint = Breakpoint(current_lineno=lineno, original_lineno=int(match.group(1)), instr=match.group(2))
-            new_breakpoints.append(breakpoint)
-    return new_breakpoints
