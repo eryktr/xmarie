bf9c7b3800061631c1119a9efc29dde57b9ee90f
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-10-22 21:40:48 +0200

    Bumped xmarie-vm to 1.6.1

--- vm_manager.py
+++ vm_manager.py
@@ -1,6 +1,8 @@
-from typing import Dict
+from typing import Dict, List, Optional
 
 from xmarievm import api
+from xmarievm.breakpoints import BreakpointHit
+from xmarievm.runtime.snapshot_maker import Snapshot
 from xmarievm.runtime.vm import MarieVm
 
 
@@ -24,7 +26,14 @@ class VmManager:
     def run(self, code: str, input_: str):
         api.run(code, debug=False, input_=input_)
 
-    def debug(self, token: str, code: str, input_: str):
+    def debug(self, token: str, code: str, input_: str, breakpoints: List[int]) -> BreakpointHit:
         self.register_client(token)
-        program = api.parse(code)
-        self.vms[token].setup_debug()
+        program = api.parse_code(code)
+        parsed_breakpoints = api.parse_breakpoints(breakpoints, code)
+        vm = self.vms[token]
+        vm.setup_debug(program, parsed_breakpoints)
+        return vm.hit_breakpoint()
+
+    def continue_debug(self, token: str) -> Optional[BreakpointHit]:
+        vm = self.vms[token]
+        return vm.hit_breakpoint()
